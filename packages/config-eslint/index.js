68315265e896450715d95d2349b39fda489e1273
Oc1S <ocis.chan@gmail.com>
2024-10-09 00:07:34 +0800

    chore: eslint config

--- packages/config-eslint/index.js
+++ packages/config-eslint/index.js
@@ -3,7 +3,26 @@ module.exports = {
   plugins: ['@typescript-eslint', 'no-only-tests', 'eslint-comments', 'simple-import-sort'],
   ignorePatterns: ['node_modules', 'dist', 'dev', 'tsup.config.ts', 'vitest.config.ts'],
   rules: {
-    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/imports': [
+      'warn',
+      {
+        groups: [
+          // Side effect imports.
+          ['^\\u0000'],
+          // Node.js builtins prefixed with `node:`.
+          ['^node:'],
+          // Packages.
+          // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+          ['^solid', '^@solid', '^@?\\w'],
+          // Absolute imports and other imports such as Vue-style `@/foo`.
+          // Anything not matched in another group.
+          ['^'],
+          // Relative imports.
+          // Anything that starts with a dot.
+          ['^\\.'],
+        ],
+      },
+    ],
     'simple-import-sort/exports': 'warn',
     'no-debugger': 'warn',
     '@typescript-eslint/no-unused-vars': [
--- packages/somo/.eslintrc.cjs
+++ packages/somo/.eslintrc.cjs
@@ -1,47 +1,9 @@
 module.exports = {
   root: true,
   extends: ['@repo/eslint-config'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'no-only-tests', 'eslint-comments', 'simple-import-sort'],
-  ignorePatterns: ['node_modules', 'dist', 'dev', 'tsup.config.ts', 'vitest.config.ts'],
   parserOptions: {
     project: './tsconfig.json',
     tsconfigRootDir: __dirname,
     sourceType: 'module',
-  },
-  rules: {
-    'simple-import-sort/imports': [
-      'warn',
-      {
-        groups: [
-          // Side effect imports.
-          ['^\\u0000'],
-          // Node.js builtins prefixed with `node:`.
-          ['^node:'],
-          // Packages.
-          // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
-          ['^solid', '^@solid', '^@?\\w'],
-          // Absolute imports and other imports such as Vue-style `@/foo`.
-          // Anything not matched in another group.
-          ['^'],
-          // Relative imports.
-          // Anything that starts with a dot.
-          ['^\\.'],
-        ],
-      },
-    ],
-    'simple-import-sort/exports': 'warn',
-    'no-debugger': 'warn',
-    '@typescript-eslint/no-unused-vars': [
-      'warn',
-      {
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-        caughtErrorsIgnorePattern: '^_',
-      },
-    ],
-    '@typescript-eslint/no-useless-empty-export': 'warn',
-    'no-only-tests/no-only-tests': 'warn',
-    'eslint-comments/no-unused-disable': 'warn',
   },
 };
